3dfea3ff706ba5f761e0872e08eda56561c99b20
allanao <ayaordonez@gmail.com>
2021-05-19 08:17:31 -0700

    localhost:8080 now runs with npm run dev command

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Search from './Search';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div id="box">
+                Hi I am alive
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     return res.status(200).send();
+// });
+
+
+// statically serve everything in the build folder on the route '/build'
+app.use('/build', express.static(path.join(__dirname, '../build')));
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+
+
+
+// ERROR HANDLERS
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send('Internal Server Error!')
+});
+
+
+
+// listens on port 3000 -> http://localhost:3000/
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
